0b9d9613ce18848f7a9bb767116ce6103f29584d
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-12-14 12:41:52 -0600

    update app.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './app/store';
+import cityListReducer from './actions/cityListReducer'
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(cityListReducer)
 
 ReactDOM.render(
   <React.StrictMode>
